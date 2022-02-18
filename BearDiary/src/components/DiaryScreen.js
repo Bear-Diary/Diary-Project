518594ad3ebd0d7047d7467dbc60659d6687a8dc
taengbae <gudqo04@gmail.com>
2022-02-18 21:18:18 +0900

    Plus/Diary
    추가

--- BearDiary/src/components/DiaryScreen.js
+++ BearDiary/src/components/DiaryScreen.js
@@ -18,7 +18,35 @@ import {
 
 function DiaryScreen({ navigation }) {
   return (
-    <View style={styles.mainBody}/>
+    <View style={styles.mainBody}>
+            <View style={styles.TopBody}>
+                <View style={styles.TopCon0}/>
+                <View style={styles.TopCon1}>
+                    <Image
+                        source={require('BearDiary/src/Bear-B.png')}
+                        style={styles.image}
+                    />
+                </View>
+                <View style={styles.TopCon2}>
+                    <Image
+                        source={require('BearDiary/src/Coin-state.png')}
+                        style={styles.image3}
+                        resizeMode="cover"
+                    />
+                </View>
+            </View>
+            <View style={styles.MidBody}>
+                <View style={styles.MidCon}>
+                    <TouchableOpacity
+                        style={styles.plusImage}
+                        onPress={() => navigation.navigate('PlusCha')}>
+                    <Image
+                        source={require('BearDiary/src/Plus.png')}
+                        style={styles.image}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+    </View>
   );
 }
 
@@ -28,5 +56,36 @@ const styles = StyleSheet.create({
   mainBody: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  TopBody: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  MidBody: {
+    flex: 6,
+    justifyContent: 'center',
+  },
+
+  image: {
+    width: 70,
+    height: 70,
+  },
+  image3: {
+    width: 160,
+    height: 40,
+  },
+  TopCon0: {
+    width: '5%',
+  },
+  TopCon1: {
+    width: '25%',
+  },
+  TopCon2: {
+    width: '50%',
+  },
+  MidCon: {
+    width: '100%',
+    alignItems: 'center',
   },
 });
